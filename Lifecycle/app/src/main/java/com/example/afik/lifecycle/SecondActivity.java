37af2b3c42e2853c0919090d354f8c0651f39bf3
afik <k.afis3@rocketmail.com>
2015-02-08 10:40:49 +0700

    lifecycle

---
+++ Lifecycle/app/src/main/java/com/example/afik/lifecycle/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.afik.lifecycle;
+
+import android.os.Bundle;
+
+public class SecondActivity extends TracerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
